e60df9411aefa21cfbc08f026201132f8db09a69
WEN Hao <wenh06@gmail.com>
2023-02-11 12:14:38 +0800

    init necessary code for the challenge

---
+++ cfg.py
@@ -0,0 +1,107 @@
+"""
+"""
+
+import pathlib
+from copy import deepcopy
+
+import numpy as np
+import torch
+from torch_ecg.cfg import CFG
+from torch_ecg.utils.utils_nn import adjust_cnn_filter_lengths  # noqa: F401
+
+from cfg_models import ModelArchCfg  # noqa: F401
+
+
+__all__ = [
+    "BaseCfg",
+    "TrainCfg",
+    "ModelCfg",
+]
+
+
+_BASE_DIR = pathlib.Path(__file__).absolute().parent
+
+
+###############################################################################
+# Base Configs,
+# including path, data type, classes, etc.
+###############################################################################
+
+BaseCfg = CFG()
+BaseCfg.db_dir = None
+BaseCfg.project_dir = _BASE_DIR
+BaseCfg.log_dir = _BASE_DIR / "log"
+BaseCfg.model_dir = _BASE_DIR / "saved_models"
+BaseCfg.log_dir.mkdir(exist_ok=True)
+BaseCfg.model_dir.mkdir(exist_ok=True)
+# BaseCfg.fs = 1000
+BaseCfg.torch_dtype = torch.float32  # "double"
+BaseCfg.np_dtype = np.float32
+BaseCfg.ignore_index = -100
+
+
+###############################################################################
+# training configurations for machine learning and deep learning
+###############################################################################
+
+TrainCfg = deepcopy(BaseCfg)
+
+###########################################
+# common configurations for all tasks
+###########################################
+
+TrainCfg.checkpoints = _BASE_DIR / "checkpoints"
+TrainCfg.checkpoints.mkdir(exist_ok=True)
+
+TrainCfg.train_ratio = 0.8
+
+# configs of training epochs, batch, etc.
+TrainCfg.n_epochs = 60
+# TODO: automatic adjust batch size according to GPU capacity
+# https://stackoverflow.com/questions/45132809/how-to-select-batch-size-automatically-to-fit-gpu
+TrainCfg.batch_size = 24
+
+# configs of optimizers and lr_schedulers
+TrainCfg.optimizer = "adamw_amsgrad"  # "sgd", "adam", "adamw"
+TrainCfg.momentum = 0.949  # default values for corresponding PyTorch optimizers
+TrainCfg.betas = (0.9, 0.999)  # default values for corresponding PyTorch optimizers
+TrainCfg.decay = 1e-2  # default values for corresponding PyTorch optimizers
+
+TrainCfg.learning_rate = 5e-4  # 1e-3
+TrainCfg.lr = TrainCfg.learning_rate
+
+TrainCfg.lr_scheduler = "one_cycle"  # "one_cycle", "plateau", "burn_in", "step", None
+TrainCfg.lr_step_size = 50
+TrainCfg.lr_gamma = 0.1
+TrainCfg.max_lr = 2e-3  # for "one_cycle" scheduler, to adjust via expriments
+
+# configs of callbacks, including early stopping, checkpoint, etc.
+TrainCfg.early_stopping = CFG()  # early stopping according to challenge metric
+TrainCfg.early_stopping.min_delta = 0.001  # should be non-negative
+TrainCfg.early_stopping.patience = TrainCfg.n_epochs // 2
+TrainCfg.keep_checkpoint_max = 10
+
+# configs of loss function
+# TrainCfg.loss = "AsymmetricLoss"  # "FocalLoss", "BCEWithLogitsLoss"
+# TrainCfg.loss_kw = CFG(gamma_pos=0, gamma_neg=0.2, implementation="deep-psp")
+TrainCfg.flooding_level = 0.0  # flooding performed if positive,
+
+# configs of logging
+TrainCfg.log_step = 20
+# TrainCfg.eval_every = 20
+
+
+def set_entry_test_flag(test_flag: bool):
+    TrainCfg.entry_test_flag = test_flag
+
+
+###############################################################################
+# configurations for building deep learning models
+# terminologies of stanford ecg repo. will be adopted
+###############################################################################
+
+_BASE_MODEL_CONFIG = CFG()
+_BASE_MODEL_CONFIG.torch_dtype = BaseCfg.torch_dtype
+
+
+ModelCfg = deepcopy(_BASE_MODEL_CONFIG)
---
+++ cfg_models.py
@@ -0,0 +1,15 @@
+"""
+"""
+
+# from copy import deepcopy
+
+from torch_ecg.cfg import CFG
+
+
+__all__ = [
+    "ModelArchCfg",
+]
+
+
+# mostly follow from torch_ecg.torch_ecg.model_configs.ecg_crnn
+ModelArchCfg = CFG()
