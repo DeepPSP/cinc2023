d8828aa37d1556b115eea3471b5118ed543f83b9
WEN Hao <wenh06@gmail.com>
2023-02-11 12:03:45 +0800

    add GitHub actions and related files

---
+++ test_docker.py
@@ -0,0 +1,17 @@
+"""
+"""
+
+################################################################################
+# NOTE comment out to test in GitHub actions
+
+# import sys
+# from pathlib import Path
+
+# sys.path.insert(0, "/home/wenhao/Jupyter/wenhao/workspace/torch_ecg/")
+# sys.path.insert(0, "/home/wenhao/Jupyter/wenhao/workspace/bib_lookup/")
+# tmp_data_dir = Path("/home/wenhao/Jupyter/wenhao/data/CinC2023/")
+################################################################################
+
+
+if __name__ == "__main__":
+    pass
