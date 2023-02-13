68c42af6fa5cadf24875b897c4fb728455e8be54
WEN Hao <wenh06@gmail.com>
2023-02-14 00:05:54 +0800

    update test files

--- test_docker.py
+++ test_docker.py
@@ -1,16 +1,26 @@
 """
 """
 
-################################################################################
-# NOTE comment out to test in GitHub actions
+import numpy as np
+import torch
 
-# import sys
-# from pathlib import Path
+from cfg import TrainCfg, ModelCfg, _BASE_DIR  # noqa: F401
+from data_reader import CINC2023Reader
 
-# sys.path.insert(0, "/home/wenhao/Jupyter/wenhao/workspace/torch_ecg/")
-# sys.path.insert(0, "/home/wenhao/Jupyter/wenhao/workspace/bib_lookup/")
-# tmp_data_dir = Path("/home/wenhao/Jupyter/wenhao/data/CinC2023/")
-################################################################################
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if ModelCfg.torch_dtype == torch.float64:
+    torch.set_default_tensor_type(torch.DoubleTensor)
+    DTYPE = np.float64
+else:
+    DTYPE = np.float32
+
+tmp_data_dir = _BASE_DIR / "tmp" / "CINC2022"
+tmp_data_dir.mkdir(parents=True, exist_ok=True)
+dr = CINC2023Reader(tmp_data_dir)
+dr.download(full=False)
+dr._ls_rec()
+del dr
 
 
 if __name__ == "__main__":
--- test_local.py
+++ test_local.py
@@ -1,17 +1,25 @@
 """
 """
 
-################################################################################
-# NOTE comment out to test in GitHub actions
-
 import sys
-
-# from pathlib import Path
+from pathlib import Path
 
 sys.path.insert(0, "/home/wenhao/Jupyter/wenhao/workspace/torch_ecg/")
 sys.path.insert(0, "/home/wenhao/Jupyter/wenhao/workspace/bib_lookup/")
-# tmp_data_dir = Path("/home/wenhao/Jupyter/wenhao/data/CinC2023/")
-################################################################################
+tmp_data_dir = Path("/home/wenhao/Jupyter/wenhao/data/CinC2023/training_subset/")
+
+import numpy as np
+import torch
+
+from cfg import TrainCfg, ModelCfg, _BASE_DIR  # noqa: F401
+
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if ModelCfg.torch_dtype == torch.float64:
+    torch.set_default_tensor_type(torch.DoubleTensor)
+    DTYPE = np.float64
+else:
+    DTYPE = np.float32
 
 
 if __name__ == "__main__":
