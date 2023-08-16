caf8dd8184c6e562793ed816404cb08f68756ca7
WEN Hao <wenh06@gmail.com>
2023-08-16 10:57:27 +0800

    update sync_official

--- sync_official.py
+++ sync_official.py
@@ -6,28 +6,38 @@ from pathlib import Path
 
 
 project_dir = Path(__file__).resolve().parent
-official_dir = project_dir / "official_baseline_classifier"
+official_dir = {
+    "baseline": project_dir / "official_baseline_classifier",
+    "scoring": project_dir / "official_scoring_metric",
+}
 
-files = [
-    "helper_code.py",
-    "remove_data.py",
-    "remove_labels.py",
-    "run_model.py",
-    "train_model.py",
-    "truncate_data.py",
-]
+files = {
+    "baseline": [
+        "helper_code.py",
+        "remove_data.py",
+        "remove_labels.py",
+        "run_model.py",
+        "train_model.py",
+        "truncate_data.py",
+    ],
+    "scoring": [
+        "evaluate_model.py",
+    ],
+}
 
 
 def main():
-    for filename in files:
-        src = official_dir / filename
-        dst = project_dir / filename
-        if src.read_text() == dst.read_text():
-            continue
-        print(
-            f"Copying **{src.relative_to(project_dir)}** to **{dst.relative_to(project_dir)}**"
-        )
-        dst.write_text(src.read_text())
+    for repo, file_list in files.items():
+        for filename in file_list:
+            src = official_dir[repo] / filename
+            dst = project_dir / filename
+            if src.read_text() == dst.read_text():
+                continue
+            print(
+                f"Copying **{src.relative_to(project_dir)}** "
+                f"to **{dst.relative_to(project_dir)}**"
+            )
+            dst.write_text(src.read_text())
 
 
 if __name__ == "__main__":
