24b3f2d14f909c26b30d0a268736bb312c9f500a
WEN Hao <wenh06@gmail.com>
2023-10-02 01:02:55 +0800

    remove the out-dated file formatting.sh; update corresponding github action file

--- formatting.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-black . -v --exclude="/build|dist|official_baseline|official_scoring_metric|artifact_pipeline|helper_code\.py|run_model\.py|train_model\.py|evaluate_model\.py|remove_data\.py|remove_labels\.py|truncate_data\.py|\.ipynb_checkpoints|fast-test.*\.ipynb"
-flake8 . --count --ignore="E501 W503 E203 F841 E402" --show-source --statistics --exclude=./.*,build,dist,official*,artifact_pipeline,helper_code.py,run_model.py,train_model.py,evaluate_model.py,remove_data.py,remove_labels.py,truncate_data.py,*.ipynb
